cf4dddf160ef0b229685bea5de52c2fa058f0dc3
changyu12 <changue5512@naver.com>
2023-11-09 14:03:07 +0900

    23-11-09
    changyu

---
+++ shopping/src/main/java/com/example/shopping/SmsService.java
@@ -0,0 +1,6 @@
+package com.example.shopping;
+
+public interface SmsService {
+
+	void sendsms(String msg, String tel);
+}
---
+++ shopping/src/main/java/com/example/shopping/SmsServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.shopping;
+
+public class SmsServiceImpl implements SmsService {
+	
+	@Override
+	public void sendsms(String msg, String tel) {
+		
+
+	}
+
+}
